5933fe61690afef086b4a59d8cbce3f9b3912331
Mohammad Alloush <mohammad.alloush963@gmail.com>
2024-03-27 02:17:57 +0400

    Post Model
    Added Post database structor

---
+++ models/Post.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+const User = require('./User');
+
+const Post = sequelize.define('Post', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+Post.belongsTo(User, { foreignKey: 'authorId', as: 'author'});
+
+module.exports = Post;
