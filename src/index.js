82d6891bc8f35c080c82730baf78f0ae79b63781
Mohammad Alloush <mohammad.alloush963@gmail.com>
2024-03-27 03:17:39 +0400

    GET /users
    Retrieve a list of all users

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ const PORT = 3500; // PORT Number
 
 app.use(bodyParser.json());
 
+// Retrieve a list of all users
+app.get('/users', async (req, res) => {
+    try {
+        const UsersList = await User.findAll();
+        res.json(UsersList);
+    } catch (err) { // error handeling
+        console.error('Error retrieving users: ', err); // show console error
+        res.status(500).send("Internal Server Error"); // reply with an error
+    }
+});
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on ${PORT} Port`);
 });
