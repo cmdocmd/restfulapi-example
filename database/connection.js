c8491ec21f93e25a1f4231da66af8009686cfad4
Mohammad Alloush <mohammad.alloush963@gmail.com>
2024-03-27 02:05:30 +0400

    MySql Configurations
    Added MySql Configurations

---
+++ database/connection.js
@@ -0,0 +1,9 @@
+const { Sequelize } = requie('sequelize');
+
+//schema name, username, password
+const sequelize = new Sequelize("nanovoltz", "root", "wfh13102003", {
+    host: "localhost",
+    dialect: "mysql"
+});
+
+module.exports = sequelize;
